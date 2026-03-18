c35ec73b42698fb9776427a8f717d29af6f45640
Yin Tirui <yintirui@gmail.com>
2026-03-18 11:09:17 +0800

    Revert "mm: Support PMD-sized pfnmaps in remap_pfn_range()"
    This reverts commit f5bde1bc1b6449abfc9ec283809c18759ec83b7a.

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1860,9 +1860,6 @@ int copy_huge_pmd(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 	pmd = pmdp_get_lockless(src_pmd);
 	if (unlikely(pmd_present(pmd) && pmd_special(pmd) &&
 		     !is_huge_zero_pmd(pmd))) {
-		pgtable = pte_alloc_one(dst_mm);
-		if (unlikely(!pgtable))
-			goto out;
 		dst_ptl = pmd_lock(dst_mm, dst_pmd);
 		src_ptl = pmd_lockptr(src_mm, src_pmd);
 		spin_lock_nested(src_ptl, SINGLE_DEPTH_NESTING);
@@ -1876,12 +1873,6 @@ int copy_huge_pmd(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 		 * able to wrongly write to the backend MMIO.
 		 */
 		VM_WARN_ON_ONCE(is_cow_mapping(src_vma->vm_flags) && pmd_write(pmd));
-
-		/* dax won't reach here, it will be intercepted at vma_needs_copy() */
-		VM_WARN_ON_ONCE(vma_is_dax(src_vma));
-
-		mm_inc_nr_ptes(dst_mm);
-		pgtable_trans_huge_deposit(dst_mm, dst_pmd, pgtable);
 		goto set_pmd;
 	}
 
@@ -2372,8 +2363,6 @@ int zap_huge_pmd(struct mmu_gather *tlb, struct vm_area_struct *vma,
 	arch_check_zapped_pmd(vma, orig_pmd);
 	tlb_remove_pmd_tlb_entry(tlb, pmd, addr);
 	if (!vma_is_dax(vma) && vma_is_special_huge(vma)) {
-		if (pmd_special(orig_pmd))
-			zap_deposited_table(tlb->mm, pmd);
 		if (arch_needs_pgtable_deposit())
 			zap_deposited_table(tlb->mm, pmd);
 		spin_unlock(ptl);
@@ -3019,35 +3008,14 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 
 	if (!vma_is_anonymous(vma)) {
 		old_pmd = pmdp_huge_clear_flush(vma, haddr, pmd);
-
-		if (!vma_is_dax(vma) && vma_is_special_huge(vma)) {
-			pte_t entry;
-
-			if (!pmd_special(old_pmd)) {
-				zap_deposited_table(mm, pmd);
-				return;
-			}
-			pgtable = pgtable_trans_huge_withdraw(mm, pmd);
-			if (unlikely(!pgtable))
-				return;
-			pmd_populate(mm, &_pmd, pgtable);
-			pte = pte_offset_map(&_pmd, haddr);
-			entry = pfn_pte(pmd_pfn(old_pmd), pmd_pgprot(old_pmd));
-			set_ptes(mm, haddr, pte, entry, HPAGE_PMD_NR);
-			pte_unmap(pte);
-
-			smp_wmb(); /* make pte visible before pmd */
-			pmd_populate(mm, pmd, pgtable);
-			return;
-		}
-
 		/*
 		 * We are going to unmap this huge page. So
 		 * just go ahead and zap it
 		 */
 		if (arch_needs_pgtable_deposit())
 			zap_deposited_table(mm, pmd);
-
+		if (!vma_is_dax(vma) && vma_is_special_huge(vma))
+			return;
 		if (unlikely(pmd_is_migration_entry(old_pmd))) {
 			const softleaf_t old_entry = softleaf_from_pmd(old_pmd);
 
